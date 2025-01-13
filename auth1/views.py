0ed9f5e4b582824f0288dbb2298b7539e67aed5f
desnica <maxim297000@gmail.com>
2025-01-13 18:04:08 +0300

    added some sort of auth

--- auth1/views.py
+++ auth1/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
 def authing(request):
-    return render(request, 'authing.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('blog1')
+        else:
+            messages.success(request, ("Username or Password is incorrect. Try again."))
+            return redirect('authing')   
+    else:  
+        return render(request, 'authing.html', {})
